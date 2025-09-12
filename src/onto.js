26cac44614371e4e81b99c859f98b4c208666966
Luca Guzzon <luca.guzzon@gmail.com>
2025-09-12 22:55:02 +0200

    ♻️ refactor: Refactor event emitter tag handling with default next and listener structure

--- src/onto.js
+++ src/onto.js
@@ -3,36 +3,63 @@
 // On event emitter generic javascript utility.
 module.exports = function onto(tag, arg, as){
 	if(!tag){ return {to: onto} }
-	var u, f = 'function' == typeof arg, tag = (this.tag || (this.tag = {}))[tag] || f && (
-		this.tag[tag] = {tag: tag, to: onto._ = { next: function(arg){ var tmp;
-			if(tmp = this.to){ tmp.next(arg) }
-	}}});
-	if(f){
-		var be = {
-			off: onto.off ||
-			(onto.off = function(){
-				if(this.next === onto._.next){ return !0 }
+	
+	var isFunction = typeof arg === 'function';
+	var defaultNext = {
+		next: function(arg){
+			var tmp = this.to;
+			if(tmp){ tmp.next(arg) }
+		}
+	};
+
+	// Get or create tag
+	var tagObj = (this.tag || (this.tag = {}))[tag];
+	if(!tagObj && isFunction){
+		tagObj = this.tag[tag] = {
+			tag: tag,
+			to: onto._ = defaultNext
+		};
+	}
+
+	// Handle function case
+	if(isFunction){
+		var listener = {
+			off: onto.off || function(){
+				if(this.next === defaultNext.next){ return true }
+				
 				if(this === this.the.last){
 					this.the.last = this.back;
 				}
+
 				this.to.back = this.back;
-				this.next = onto._.next;
+				this.next = defaultNext.next;
 				this.back.to = this.to;
+
 				if(this.the.last === this.the){
 					delete this.on.tag[this.the.tag];
 				}
-			}),
-			to: onto._,
+			},
+			to: defaultNext,
 			next: arg,
-			the: tag,
+			the: tagObj,
 			on: this,
-			as: as,
+			as: as
 		};
-		(be.back = tag.last || tag).to = be;
-		return tag.last = be;
+
+		var last = tagObj.last || tagObj;
+		listener.back = last;
+		last.to = listener;
+		tagObj.last = listener;
+
+		return listener;
 	}
-	if((tag = tag.to) && u !== arg){ tag.next(arg) }
-	return tag;
+
+	// Handle non-function case
+	var target = tagObj && tagObj.to;
+	if(target && arg !== undefined){
+		target.next(arg);
+	}
+	return target;
 };
 	
 }());
