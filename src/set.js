4da709d3d4fdec414698ed41321025a977da199f
Luca Guzzon <luca.guzzon@gmail.com>
2025-09-12 22:55:02 +0200

    ♻️ refactor: modernize variable declarations and item handling with async/await

--- src/set.js
+++ src/set.js
@@ -1,24 +1,51 @@
 ;(function(){
 
-var Gun = require('./root');
-Gun.chain.set = function(item, cb, opt){
-	var gun = this, root = gun.back(-1), soul, tmp;
-	cb = cb || function(){};
-	opt = opt || {}; opt.item = opt.item || item;
-	if(soul = ((item||'')._||'')['#']){ (item = {})['#'] = soul } // check if node, make link.
-	if('string' == typeof (tmp = Gun.valid(item))){ return gun.get(soul = tmp).put(item, cb, opt) } // check if link
-	if(!Gun.is(item)){
-		if(Object.plain(item)){
-			item = root.get(soul = gun.back('opt.uuid')()).put(item);
-		}
-		return gun.get(soul || root.back('opt.uuid')(7)).put(item, cb, opt);
+const Gun = require('./root');
+Gun.chain.set = function(item, cb = () => {}, opt = {}){
+	const gun = this;
+	const root = gun.back(-1);
+	let soul;
+
+	opt.item = opt.item || item;
+
+	// If item is a node, extract soul and convert to link
+	if (item?._?.['#']) {
+		soul = item._['#'];
+		item = {'#': soul};
 	}
-	gun.put(function(go){
-		item.get(function(soul, o, msg){ // TODO: BUG! We no longer have this option? & go error not handled?
-			if(!soul){ return cb.call(gun, {err: Gun.log('Only a node can be linked! Not "' + msg.put + '"!')}) }
-			(tmp = {})[soul] = {'#': soul}; go(tmp);
-		},true);
-	})
+
+	// Check if item is a valid link
+	const tmp = Gun.valid(item);
+	if (typeof tmp === 'string') { 
+		return gun.get(soul = tmp).put(item, cb, opt);
+	}
+
+	// Handle non-Gun items - plain objects and other values
+	if (!Gun.is(item)) {
+		if (Object.plain(item)) {
+			soul = gun.back('opt.uuid')();
+			item = root.get(soul).put(item);
+		}
+		soul = soul || root.back('opt.uuid')(7);
+		return gun.get(soul).put(item, cb, opt);
+	}
+
+	// Handle Gun items - get soul and create link
+	gun.put(async (go) => {
+		item.get((soul, o, msg) => {
+			// Error if no valid soul found
+			if (!soul) {
+				return cb.call(gun, {
+					err: Gun.log(`Only a node can be linked! Not "${msg.put}"!`)
+				});
+			}
+			// Create the link object
+			const tmp = {};
+			tmp[soul] = {'#': soul};
+			go(tmp);
+		}, true);
+	});
+
 	return item;
 }
 	
