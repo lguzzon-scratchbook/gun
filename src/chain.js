6efb3e858008ee36650b2d448ce419868d46a882
Luca Guzzon <luca.guzzon@gmail.com>
2025-09-18 10:59:22 +0200

    feat: lint src/chain.js

--- src/chain.js
+++ src/chain.js
@@ -1,14 +1,14 @@
-;(function(){
+;((()=> {
 
 // WARNING: GUN is very simple, but the JavaScript chaining API around GUN
 // is complicated and was extremely hard to build. If you port GUN to another
 // language, consider implementing an easier API to build.
 var Gun = require('./root');
 Gun.chain.chain = function(sub){
-	var gun = this, at = gun._, chain = new (sub || gun).constructor(gun), cat = chain._, root;
+	var at = this._, chain = new (sub || this).constructor(this), cat = chain._, root;
 	cat.root = root = at.root;
 	cat.id = ++root.once;
-	cat.back = gun._;
+	cat.back = this._;
 	cat.on = Gun.on;
 	cat.on('in', Gun.on.in, cat); // For 'in' if I add my own listeners to each then I MUST do it before in gets called. If I listen globally for all incoming data instead though, regardless of individual listeners, I can transform the data there and then as well.
 	cat.on('out', Gun.on.out, cat); // However for output, there isn't really the global option. I must listen by adding my own listener individually BEFORE this one is ever called.
@@ -26,7 +26,7 @@ function output(msg){
 			return;
 		}*/
 		if(root.pass){ root.pass[at.id] = at; } // will this make for buggy behavior elsewhere?
-		if(at.lex){ Object.keys(at.lex).forEach(function(k){ tmp[k] = at.lex[k] }, tmp = msg.get = msg.get || {}) }
+		if(at.lex){ Object.keys(at.lex).forEach((k)=> { tmp[k] = at.lex[k] }, tmp = msg.get = msg.get || {}) }
 		if(get['#'] || at.soul){
 			get['#'] = get['#'] || at.soul;
 			//root.graph[get['#']] = root.graph[get['#']] || {_:{'#':get['#'],'>':{}}};
@@ -103,7 +103,7 @@ function input(msg, cat){ cat = cat || this.as; // TODO: V8 may not be able to o
 		if(!valid(tmp)){
 			if(!(soul = ((tmp||'')._||'')['#'])){ console.log("chain not yet supported for", tmp, '...', msg, cat); return; }
 			gun = cat.root.$.get(soul);
-			return setTimeout.each(Object.keys(tmp).sort(), function(k){ // TODO: .keys( is slow // BUG? ?Some re-in logic may depend on this being sync?
+			return setTimeout.each(Object.keys(tmp).sort(), (k)=> { // TODO: .keys( is slow // BUG? ?Some re-in logic may depend on this being sync?
 				if('_' == k || u === (state = state_is(tmp, k))){ return }
 				cat.on('in', {$: gun, put: {'#': soul, '.': k, '=': tmp[k], '>': state}, VIA: msg});
 			});
@@ -111,10 +111,10 @@ function input(msg, cat){ cat = cat || this.as; // TODO: V8 may not be able to o
 		cat.on('in', {$: at.back.$, put: {'#': soul = at.back.soul, '.': key = at.has || at.get, '=': tmp, '>': state_is(at.back.put, key)}, via: msg}); // TODO: This could be buggy! It assumes/approxes data, other stuff could have corrupted it.
 		return;
 	}
-	if((msg.seen||'')[cat.id]){ return } (msg.seen || (msg.seen = function(){}))[cat.id] = cat; // help stop some infinite loops
+	if((msg.seen||'')[cat.id]){ return } (msg.seen || (msg.seen = ()=> {}))[cat.id] = cat; // help stop some infinite loops
 
 	if(cat !== at){ // don't worry about this when first understanding the code, it handles changing contexts on a message. A soul chain will never have a different context.
-		Object.keys(msg).forEach(function(k){ tmp[k] = msg[k] }, tmp = {}); // make copy of message
+		Object.keys(msg).forEach((k)=> { tmp[k] = msg[k] }, tmp = {}); // make copy of message
 		tmp.get = cat.get || tmp.get;
 		if(!cat.soul && !cat.has){ // if we do not recognize the chain type
 			tmp.$$$ = tmp.$$$ || cat.$; // make a reference to wherever it came from.
@@ -139,13 +139,13 @@ function input(msg, cat){ cat = cat || this.as; // TODO: V8 may not be able to o
 
 	this.to && this.to.next(msg); // 1st API job is to call all chain listeners.
 	// TODO: Make input more reusable by only doing these (some?) calls if we are a chain we recognize? This means each input listener would be responsible for when listeners need to be called, which makes sense, as they might want to filter.
-	cat.any && setTimeout.each(Object.keys(cat.any), function(any){ (any = cat.any[any]) && any(msg) },0,99); // 1st API job is to call all chain listeners. // TODO: .keys( is slow // BUG: Some re-in logic may depend on this being sync.
-	cat.echo && setTimeout.each(Object.keys(cat.echo), function(lat){ (lat = cat.echo[lat]) && lat.on('in', msg) },0,99); // & linked at chains // TODO: .keys( is slow // BUG: Some re-in logic may depend on this being sync.
+	cat.any && setTimeout.each(Object.keys(cat.any), (any)=> { (any = cat.any[any]) && any(msg) },0,99); // 1st API job is to call all chain listeners. // TODO: .keys( is slow // BUG: Some re-in logic may depend on this being sync.
+	cat.echo && setTimeout.each(Object.keys(cat.echo), (lat)=> { (lat = cat.echo[lat]) && lat.on('in', msg) },0,99); // & linked at chains // TODO: .keys( is slow // BUG: Some re-in logic may depend on this being sync.
 
 	if(((msg.$$||'')._||at).soul){ // comments are linear, but this line of code is non-linear, so if I were to comment what it does, you'd have to read 42 other comments first... but you can't read any of those comments until you first read this comment. What!? // shouldn't this match link's check?
 		// is there cases where it is a $$ that we do NOT want to do the following? 
 		if((sat = cat.next) && (sat = sat[key])){ // TODO: possible trick? Maybe have `ionmap` code set a sat? // TODO: Maybe we should do `cat.ask` instead? I guess does not matter.
-			tmp = {}; Object.keys(msg).forEach(function(k){ tmp[k] = msg[k] });
+			tmp = {}; Object.keys(msg).forEach((k)=> { tmp[k] = msg[k] });
 			tmp.$ = (msg.$$||msg.$).get(tmp.get = key); delete tmp.$$; delete tmp.$$$;
 			sat.on('in', tmp);
 		}
@@ -176,7 +176,7 @@ function link(msg, cat){ cat = cat || this.as || msg.$._;
 	if(tmp[''] || cat.lex){ // we might need to load the whole thing // TODO: cat.lex probably has edge case bugs to it, need more test coverage.
 		sat.on('out', {get: {'#': link}});
 	}
-	setTimeout.each(Object.keys(tmp), function(get, sat){ // if sub chains are asking for data. // TODO: .keys( is slow // BUG? ?Some re-in logic may depend on this being sync?
+	setTimeout.each(Object.keys(tmp), (get, sat)=> { // if sub chains are asking for data. // TODO: .keys( is slow // BUG? ?Some re-in logic may depend on this being sync?
 		if(!get || !(sat = tmp[get])){ return }
 		sat.on('out', {get: {'#': link, '.': get}}); // go get it.
 	},0,99);
@@ -200,7 +200,7 @@ function unlink(msg, cat){ // ugh, so much code for seemingly edge case behavior
 		}
 		cat.put = u; // empty out the cache if, for example, alice's car's color no longer exists (relative to alice) if alice no longer has a car.
 		// TODO: BUG! For maps, proxy this so the individual sub is triggered, not all subs.
-		setTimeout.each(Object.keys(cat.next||''), function(get, sat){ // empty out all sub chains. // TODO: .keys( is slow // BUG? ?Some re-in logic may depend on this being sync? // TODO: BUG? This will trigger deeper put first, does put logic depend on nested order? // TODO: BUG! For map, this needs to be the isolated child, not all of them.
+		setTimeout.each(Object.keys(cat.next||''), (get, sat)=> { // empty out all sub chains. // TODO: .keys( is slow // BUG? ?Some re-in logic may depend on this being sync? // TODO: BUG? This will trigger deeper put first, does put logic depend on nested order? // TODO: BUG! For map, this needs to be the isolated child, not all of them.
 			if(!(sat = cat.next[get])){ return }
 			//if(cat.has && u === sat.put && !(root.pass||'')[sat.id]){ return } // if we are already unlinked, do not call again, unless edge case. // TODO: BUG! This line should be deleted for "unlink deeply nested".
 			if(link){ delete (root.$.get(link).get(get)._.echo||'')[sat.id] }
@@ -248,6 +248,6 @@ function ack(msg, ev){
 	return; // eom
 }
 
-var empty = {}, u, text_rand = String.random, valid = Gun.valid, obj_has = function(o, k){ return o && Object.prototype.hasOwnProperty.call(o, k) }, state = Gun.state, state_is = state.is, state_ify = state.ify;
+var empty = {}, u, text_rand = String.random, valid = Gun.valid, obj_has = (o, k)=> o && Object.hasOwn(o, k), state = Gun.state, state_is = state.is, state_ify = state.ify;
 	
-}());
+})());
