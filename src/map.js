087e3ccc365f94c12326e84dfa6bca7bd14bb8e7
Luca Guzzon <luca.guzzon@gmail.com>
2025-09-18 11:06:44 +0200

    feat: lint src/map.js

--- src/map.js
+++ src/map.js
@@ -1,7 +1,7 @@
-;(function(){
+;((()=> {
 
 var Gun = require('./root'), next = Gun.chain.get.next;
-Gun.chain.get.next = function(gun, lex){ var tmp;
+Gun.chain.get.next = (gun, lex)=> { var tmp;
 	if(!Object.plain(lex)){ return (next||noop)(gun, lex) }
 	if(tmp = ((tmp = lex['#'])||'')['='] || tmp){ return gun.get(tmp) }
 	(tmp = gun.chain()._).lex = lex; // LEX!
@@ -14,23 +14,23 @@ Gun.chain.get.next = function(gun, lex){ var tmp;
 	return tmp.$;
 }
 Gun.chain.map = function(cb, opt, t){
-	var gun = this, cat = gun._, lex, chain;
+	var cat = this._, lex, chain;
 	if(Object.plain(cb)){ lex = cb['.']? cb : {'.': cb}; cb = u }
 	if(!cb){
 		if(chain = cat.each){ return chain }
-		(cat.each = chain = gun.chain())._.lex = lex || chain._.lex || cat.lex;
-		chain._.nix = gun.back('nix');
-		gun.on('in', map, chain._);
+		(cat.each = chain = this.chain())._.lex = lex || chain._.lex || cat.lex;
+		chain._.nix = this.back('nix');
+		this.on('in', map, chain._);
 		return chain;
 	}
 	Gun.log.once("mapfn", "Map functions are experimental, their behavior and API may change moving forward. Please play with it and report bugs and ideas on how to improve it.");
-	chain = gun.chain();
-	gun.map().on(function(data, key, msg, eve){
+	chain = this.chain();
+	this.map().on(function(data, key, msg, eve){
 		var next = (cb||noop).call(this, data, key, msg, eve);
 		if(u === next){ return }
 		if(data === next){ return chain._.on('in', msg) }
 		if(Gun.is(next)){ return chain._.on('in', next._) }
-		var tmp = {}; Object.keys(msg.put).forEach(function(k){ tmp[k] = msg.put[k] }, tmp); tmp['='] = next; 
+		var tmp = {}; Object.keys(msg.put).forEach((k)=> { tmp[k] = msg.put[k] }, tmp); tmp['='] = next; 
 		chain._.on('in', {get: key, put: tmp});
 	});
 	return chain;
@@ -41,6 +41,6 @@ function map(msg){ this.to.next(msg);
 	if((tmp = cat.lex) && !String.match(msg.get|| (put||'')['.'], tmp['.'] || tmp['#'] || tmp)){ return }
 	Gun.on.link(msg, cat);
 }
-var noop = function(){}, event = {stun: noop, off: noop}, u;
+var noop = ()=> {}, event = {stun: noop, off: noop}, u;
 	
-}());
+})());
