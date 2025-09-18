d28422563193330861ce6fce1ecd6f5ee2467851
Luca Guzzon <luca.guzzon@gmail.com>
2025-09-18 11:10:27 +0200

    feat: format src/polyfill/unbuild.js

--- src/polyfill/unbuild.js
+++ src/polyfill/unbuild.js
@@ -1,12 +1,16 @@
-
-  function USE(arg, req){
-    return req? require(arg) : arg.slice? USE[R(arg)] : function(mod, path){
-      arg(mod = {exports: {}});
-      USE[R(path)] = mod.exports;
-    }
-    function R(p){
-      return p.split('/').slice(-1).toString().replace('.js','');
-    }
+function USE(arg, req) {
+  return req
+    ? require(arg)
+    : arg.slice
+      ? USE[R(arg)]
+      : function (mod, path) {
+          arg((mod = { exports: {} }))
+          USE[R(path)] = mod.exports
+        }
+  function R(p) {
+    return p.split('/').slice(-1).toString().replace('.js', '')
   }
-  if(typeof module !== "undefined"){ var MODULE = module }
-  
+}
+if (typeof module !== 'undefined') {
+  var MODULE = module
+}
