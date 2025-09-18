4f685ae9f0fbd06a065c0d157cbcbf7eb0ebf279
Luca Guzzon <luca.guzzon@gmail.com>
2025-09-18 11:14:37 +0200

    feat: lint src/shim.js

--- src/shim.js
+++ src/shim.js
@@ -1,14 +1,14 @@
-;(function(){
+;((()=> {
 
 // Shim for generic javascript utilities.
-String.random = function(l, c){
+String.random = (l, c)=> {
 	var s = '';
 	l = l || 24; // you are not going to make a 0 length random number, so no need to check type
 	c = c || '0123456789ABCDEFGHIJKLMNOPQRSTUVWXZabcdefghijklmnopqrstuvwxyz';
 	while(l-- > 0){ s += c.charAt(Math.floor(Math.random() * c.length)) }
 	return s;
 }
-String.match = function(t, o){ var tmp, u;
+String.match = (t, o)=> { var tmp, u;
 	if('string' !== typeof t){ return false }
 	if('string' == typeof o){ o = {'=': o} }
 	o = o || {};
@@ -25,7 +25,7 @@ String.match = function(t, o){ var tmp, u;
 	if(u !== o['<'] && t <= o['<']){ return true }
 	return false;
 }
-String.hash = function(s, c){ // via SO
+String.hash = (s, c)=> { // via SO
 	if(typeof s !== 'string'){ return }
 	    c = c || 0; // CPU schedule hashing by
 	    if(!s.length){ return c }
@@ -37,33 +37,33 @@ String.hash = function(s, c){ // via SO
 	    return c;
 	  }
 var has = Object.prototype.hasOwnProperty;
-Object.plain = function(o){ return o? (o instanceof Object && o.constructor === Object) || Object.prototype.toString.call(o).match(/^\[object (\w+)\]$/)[1] === 'Object' : false }
-Object.empty = function(o, n){
+Object.plain = (o)=> o? (o instanceof Object && o.constructor === Object) || Object.prototype.toString.call(o).match(/^\[object (\w+)\]$/)[1] === 'Object' : false
+Object.empty = (o, n)=> {
 	for(var k in o){ if(has.call(o, k) && (!n || -1==n.indexOf(k))){ return false } }
 	return true;
 }
-Object.keys = Object.keys || function(o){
+Object.keys = Object.keys || ((o)=> {
 	var l = [];
 	for(var k in o){ if(has.call(o, k)){ l.push(k) } }
 	return l;
-}
-;(function(){
+})
+;((()=> {
 	var u, sT = setTimeout, l = 0, c = 0
-	, sI = (typeof setImmediate !== ''+u && setImmediate) || (function(c,f){
+	, sI = (typeof setImmediate !== ''+u && setImmediate) || (((c,f)=> {
 		if(typeof MessageChannel == ''+u){ return sT }
-		(c = new MessageChannel()).port1.onmessage = function(e){ ''==e.data && f() }
-		return function(q){ f=q;c.port2.postMessage('') }
-	}()), check = sT.check = sT.check || (typeof performance !== ''+u && performance)
-	|| {now: function(){ return +new Date }};
+		(c = new MessageChannel()).port1.onmessage = (e)=> { ''==e.data && f() }
+		return (q)=> { f=q;c.port2.postMessage('') }
+	})()), check = sT.check = sT.check || (typeof performance !== ''+u && performance)
+	|| {now: ()=> +new Date};
 	sT.hold = sT.hold || 9; // half a frame benchmarks faster than < 1ms?
-	sT.poll = sT.poll || function(f){
+	sT.poll = sT.poll || ((f)=> {
 		if((sT.hold >= (check.now() - l)) && c++ < 3333){ f(); return }
-		sI(function(){ l = check.now(); f() },c=0)
-	}
-}());
-;(function(){ // Too many polls block, this "threads" them in turns over a single thread in time.
-	var sT = setTimeout, t = sT.turn = sT.turn || function(f){ 1 == s.push(f) && p(T) }
-	, s = t.s = [], p = sT.poll, i = 0, f, T = function(){
+		sI(()=> { l = check.now(); f() },c=0)
+	})
+})());
+;((()=> { // Too many polls block, this "threads" them in turns over a single thread in time.
+	var sT = setTimeout, t = sT.turn = sT.turn || ((f)=> { 1 == s.push(f) && p(T) })
+	, s = t.s = [], p = sT.poll, i = 0, f, T = ()=> {
 		if(f = s[i++]){ f() }
 		if(i == s.length || 99 == i){
 			s = t.s = s.slice(i);
@@ -71,17 +71,17 @@ Object.keys = Object.keys || function(o){
 		}
 		if(s.length){ p(T) }
 	}
-}());
-;(function(){
+})());
+;((()=> {
 	var u, sT = setTimeout, T = sT.turn;
-	(sT.each = sT.each || function(l,f,e,S){ S = S || 9; (function t(s,L,r){
+	(sT.each = sT.each || ((l,f,e,S)=> { S = S || 9; (function t(s,L,r){
 	  if(L = (s = (l||[]).splice(0,S)).length){
 	  	for(var i = 0; i < L; i++){
 	  		if(u !== (r = f(s[i]))){ break }
 	  	}
 	  	if(u === r){ T(t); return }
 	  } e && e(r);
-	}())})();
-}());
+	}())}))();
+})());
 	
-}());
+})());
