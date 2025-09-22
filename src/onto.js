1f15cfa4ace1cac371652a3729362e951f762730
Luca Guzzon <luca.guzzon@gmail.com>
2025-09-22 19:04:42 +0200

    refactor(src/onto.js): modernize event emitter with JSDoc and modular structure
    - Refactored minified code into readable functions with descriptive names
    - Added comprehensive JSDoc comments for better documentation
    - Extracted event registration and removal into separate functions
    - Maintained existing API while improving code maintainability and clarity

--- src/onto.js
+++ src/onto.js
@@ -1,55 +1,136 @@
 ;(() => {
-  // On event emitter generic javascript utility.
-  module.exports = function onto(tag, arg, as) {
-    if (!tag) {
+  /**
+   * Event Emitter Utility - A lightweight event system for JavaScript
+   *
+   * This module provides a simple event emitter implementation that allows:
+   * - Registering event listeners
+   * - Emitting events to registered listeners
+   * - Removing event listeners
+   * - Chaining event listeners
+   *
+   * @module EventEmitter
+   */
+
+  /**
+   * Main event emitter function that handles both event registration and emission
+   *
+   * @param {string} eventName - The name of the event to listen to or emit
+   * @param {Function|*} handler - Event handler function or data to emit
+   * @param {*} context - Optional context for the event handler
+   * @returns {Object|Function} Returns event system object or listener for chaining
+   */
+  function onto(eventName, handler, context) {
+    // If no event name provided, return the onto function for chaining
+    if (!eventName) {
       return { to: onto }
     }
-    var u,
-      f = 'function' == typeof arg,
-      tag =
-        (this.tag || (this.tag = {}))[tag] ||
-        (f &&
-          (this.tag[tag] = {
-            tag: tag,
-            to: (onto._ = {
-              next: function (arg) {
-                var tmp
-                if ((tmp = this.to)) {
-                  tmp.next(arg)
-                }
-              }
-            })
-          }))
-    if (f) {
-      var be = {
-        as: as,
-        next: arg,
-        off:
-          onto.off ||
-          (onto.off = function () {
-            if (this.next === onto._.next) {
-              return !0
-            }
-            if (this === this.the.last) {
-              this.the.last = this.back
-            }
-            this.to.back = this.back
-            this.next = onto._.next
-            this.back.to = this.to
-            if (this.the.last === this.the) {
-              delete this.on.tag[this.the.tag]
-            }
-          }),
-        on: this,
-        the: tag,
-        to: onto._
+
+    const isFunction = typeof handler === 'function'
+
+    // Initialize event registry if it doesn't exist
+    this.tag ??= {}
+
+    // Get existing event or create new one if handler is a function
+    let eventRegistry = this.tag[eventName]
+
+    if (!eventRegistry && isFunction) {
+      // Create new event registry with terminal node
+      eventRegistry = this.tag[eventName] = {
+        tag: eventName,
+        to: onto.terminalNode
       }
-      ;(be.back = tag.last || tag).to = be
-      return (tag.last = be)
     }
-    if ((tag = tag.to) && u !== arg) {
-      tag.next(arg)
+
+    // If handler is a function, register it as a listener
+    if (isFunction) {
+      return registerEventListener(eventRegistry, handler, context, this)
     }
-    return tag
+
+    // If handler is not a function, emit the event
+    if (eventRegistry?.to && handler !== undefined) {
+      eventRegistry.to.next(handler)
+    }
+
+    return eventRegistry?.to
+  }
+
+  /**
+   * Terminal node for the event chain - handles the end of the listener chain
+   */
+  onto.terminalNode = {
+    next(data) {
+      // Pass data to next listener in chain if it exists
+      this.to?.next(data)
+    }
+  }
+
+  /**
+   * Registers a new event listener in the event chain
+   *
+   * @param {Object} eventRegistry - The event registry object
+   * @param {Function} handler - The event handler function
+   * @param {*} context - Optional context for the handler
+   * @param {Object} emitter - The event emitter instance
+   * @returns {Object} The new listener object
+   */
+  function registerEventListener(eventRegistry, handler, context, emitter) {
+    const listener = {
+      as: context,
+      next: handler,
+      off: removeListener,
+      on: emitter,
+      the: eventRegistry,
+      to: onto.terminalNode
+    }
+
+    // Link the new listener into the chain
+    const previousLast = eventRegistry.last || eventRegistry
+    listener.back = previousLast
+    previousLast.to = listener
+    eventRegistry.last = listener
+
+    return listener
+  }
+
+  /**
+   * Removes a listener from the event chain
+   * This function is bound to each listener object as the 'off' method
+   *
+   * @returns {boolean} True if listener was already removed
+   */
+  function removeListener() {
+    // Check if already removed
+    if (this.next === onto.terminalNode.next) {
+      return true
+    }
+
+    // Update the last pointer if this is the last listener
+    if (this === this.the.last) {
+      this.the.last = this.back
+    }
+
+    // Remove from the chain by linking previous to next
+    this.to.back = this.back
+    this.back.to = this.to
+
+    // Mark as removed
+    this.next = onto.terminalNode.next
+
+    // Clean up event registry if no more listeners
+    if (this.the.last === this.the) {
+      delete this.on.tag[this.the.tag]
+    }
+
+    return false
+  }
+
+  // Attach the removal function to the main onto function for access
+  onto.off = removeListener
+
+  // Export the module
+  if (module?.exports) {
+    module.exports = onto
+  } else if (typeof window !== 'undefined') {
+    window.onto = onto
   }
 })()
